152353fc5a499c65976049611d33aa8cc357bf09
youketero <youketeroamano22@gmail.com>
2017-03-13 12:08:06 +0200

    get_short_text method added

--- gis/models.py
+++ gis/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+text_len = 700
+
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +13,9 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_short_text(self):
+        if len(self.text)>text_len:
+            return self.text[:text_len]
+        else:
+            return self.text
