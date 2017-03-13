364ddee5b2ff0a29b2df55cb61db0b9ab4d97c84
youketero <youketeroamano22@gmail.com>
2017-03-13 11:45:29 +0200

    Articles  was added in Home. About urls, views, teemplates were created

--- gis/urls.py
+++ gis/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 
 import  gis.views
 urlpatterns = [
-    url(r'^$', gis.views.home, name ="home")
+    url(r'^$', gis.views.home, name ="home"),
+    url(r'^about/$', gis.views.about, name ="about"),
 ]
